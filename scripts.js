35bfd80291b2fd7761c1a699fafc995116350a7a
Franz <franzj.pretorius.2003@gmail.com>
2024-04-05 09:54:22 +0200

    Completed extractBiggest function

--- scripts.js
+++ scripts.js
@@ -10,22 +10,42 @@ const data = {
 
 // Only edit below
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+//Grabs each number array
+const first = data.lists[0][1]
+const second = data.lists[1][1]
+const third = data.lists[2][1]
 
+//Empty string for result
 const result = []
 
-const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
+//Variables for the last element of the array's index
+let firstCount = data.lists[0][1].length -1
+let secondCount = data.lists[1][1].length -1
+let thirdCount = data.lists[2][1].length -1
 
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
+//Compares the last element of the 3 arrays and gives the highest value
+const extractBiggest = () => {
+
+    //If statement comparing first and second arrays's last element
+    if (first[firstCount] > second[secondCount]) {
+        //If first array's element is bigger it returns it and decreases the variable that keeps track of the last element's index
+        firstCount -= 1;
+        return first[firstCount + 1]
+
+    }else {
+        //If statement comparing second and third arrays's last element
+        if (second[secondCount] > third[thirdCount]) {
+            //If second array's element is bigger it returns it and decreases the variable that keeps track of the last element's index
+            secondCount -= 1;
+            return second[secondCount + 1]
+
+        }else {
+            //Returns third array's last element and decreases the variable that keeps track of the last element's index
+            thirdCount -= 1;
+            return third[thirdCount + 1]
+
+        }
+    }
 }
 
 // Only edit above
